8df690ca2594e0a59b2ea912dbf8b52b6e582bf3
Tommy Janszen <tommy.janszen@gmail.com>
2015-02-12 15:45:16 -0800

    Add sorting functinality

--- client/main.js
+++ client/main.js
@@ -1,1 +1,25 @@
-console.log('hello, world');
+'use strict';
+
+$(document).ready(init);
+
+function init() {
+  $('#get-data').click(clickGetData);
+}
+
+function clickGetData() {
+  var year = $('#year').val();
+  var info = $('#type').val();
+  console.log(info);
+  var url = 'https://cdph.data.ca.gov/resource/kbup-p858.json?year=' + year;
+  $.getJSON(url, function(response) {
+
+
+
+    var deaths = _.map(response, function(o){return o[info] * 1});
+
+    deaths = _.compact(deaths);
+    deaths = _.reduce(deaths, function(prev, curr){return prev+curr})
+
+    console.log(deaths)
+  });
+}
